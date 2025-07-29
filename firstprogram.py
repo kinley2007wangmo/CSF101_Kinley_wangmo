2109cf82c64705a1ff694f7b6432c6e51688005a
kinley2007wangmo <kw8384457@gmail.com>
2025-07-29 06:58:39 -0700

    Upload correct files

---
+++ firstprogram.py
@@ -0,0 +1,10 @@
+print("Hello SWE")
+a=2
+b=3
+sum=a+b
+print(sum)
+print(a+b)
+print ("The sum of a and b is:",sum)
+name = input("Enter your name")
+age = input("Enter your age")
+print("You are", name, "you are age",age,"years old")
